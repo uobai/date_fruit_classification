44a99f4c15446c4aebb32bf5b666f01a409d8fb6
Stanislaw Cronberg <stanislawcronberg@gmail.com>
2022-06-05 08:24:21 +0200

    Fix incorrect strings in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Repository for students from MSc AI Jan '22 cohort to collaborate on classification of date fruits from a kaggle competition.',
-    author='University of Bath MSc AI Jan '22',
+    description="Repository for students from MSc AI Jan '22 cohort to collaborate on classification of date fruits "
+                "from a kaggle competition.",
+    author="University of Bath MSc AI Jan '22",
     license='MIT',
 )
